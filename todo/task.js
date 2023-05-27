b92f127a336c9db6d830f9092afd63a6aa0733bf
Grachev Maksim <magra85@gmail.com>
2023-05-27 21:20:41 +0300

    Added HW 3.3

---
+++ todo/task.js
@@ -0,0 +1,20 @@
+const taskInput = document.getElementById('task__input');
+const tasksAdd = document.getElementById('tasks__add');
+const taskList = document.getElementById('tasks__list');
+
+tasksAdd.addEventListener('click', function (event) {    
+    event.preventDefault(); 
+
+    if (taskInput.value.trim() === '') return;
+    
+    taskList.innerHTML += `
+    <div class="task">
+        <div class="task__title">
+            ${taskInput.value}
+        </div>
+        <a href="#" class="task__remove" onclick="this.closest('.task').outerHTML = '';">&times;</a>
+    </div>        
+    `;
+
+    taskInput.value = '';   
+});
